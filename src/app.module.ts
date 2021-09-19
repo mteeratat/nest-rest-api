f46afaddb61ab7f4f127032feb9011ed7d7eae70
M CPE <mmmiko@hotmail.com>
2021-09-20 03:03:45 +0700

    25.49

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemController } from './item/item.controller';
+import { ItemsService } from './items/items.service';
+import { ItemService } from './item/item.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ItemController],
+  providers: [AppService, ItemsService, ItemService],
 })
 export class AppModule {}
